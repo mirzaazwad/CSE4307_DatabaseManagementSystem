be09ee39c460b70f768f64d0a250670cbb4ffad7
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-10-20 18:43:16 +0600

    Update to DDL

--- Lab-07/DDL.sql
+++ Lab-07/DDL.sql
@@ -1,3 +1,13 @@
+drop table ACCIDENT_LICENSE;
+drop table ACCIDENT;
+drop table LICENSE;
+drop table PATIENT;
+drop table CITIZEN;
+drop table DISTRICT;
+drop table DIVISION;
+drop table HOSPITAL;
+
+
 CREATE TABLE CITIZEN(
   NID varchar2(12),
   Name varchar2(100),
@@ -39,26 +49,29 @@ CREATE TABLE LICENSE(
 CREATE TABLE ACCIDENT(
   dateOfAccident date,
   timeOfAccident timestamp,
-  location varchar2(20),
+  DistrictName varchar2(20),
+  DistrictSize number,
   numberOfDeaths INTEGER,
-  constraint PK_ACCIDENT PRIMARY KEY(dateOfAccident, timeOfAccident, location)
+  constraint PK_ACCIDENT PRIMARY KEY(dateOfAccident, timeOfAccident, DistrictName,DistrictSize)
 );
 
 CREATE TABLE ACCIDENT_LICENSE(
   dateOfAccident date,
   timeOfAccident timestamp,
-  location varchar2(20),
+  DistrictName varchar2(20),
+  DistrictSize number,
   NID varchar2(12),
-  constraint PK_ACCIDENT_LICENSE PRIMARY KEY(dateOfAccident, timeOfAccident,location,NID),
+  constraint PK_ACCIDENT_LICENSE PRIMARY KEY(dateOfAccident, timeOfAccident,DistrictName,DistrictSize,NID),
   constraint FK_ACCIDENT_LICENSE_LICENSE FOREIGN KEY(NID) REFERENCES LICENSE(NID),
-  constraint FK_ACCIDENT_LICENSE_ACCIDENT FOREIGN KEY(dateOfAccident, timeOfAccident,location) REFERENCES ACCIDENT(dateOfAccident, timeOfAccident,location)
+  constraint FK_ACCIDENT_LICENSE_ACCIDENT FOREIGN KEY(dateOfAccident, timeOfAccident,DistrictName,DistrictSize) REFERENCES ACCIDENT(dateOfAccident, timeOfAccident,DistrictName,DistrictSize)
 );
 
 CREATE TABLE HOSPITAL(
-  Name varchar2(100),
+  HospitalName varchar2(100),
+  HospitalDistrictName varchar2(20),
+  HospitalDistrictSize number,
   ContactInformation vmobiles,
-  Location varchar2(20),
-  constraint PK_HOSPITAL PRIMARY KEY(Name, Location)
+  constraint PK_HOSPITAL PRIMARY KEY(HospitalName,HospitalDistrictName,HospitalDistrictSize)
 );
 
 CREATE TABLE PATIENT(
@@ -67,8 +80,10 @@ CREATE TABLE PATIENT(
   BriefDescription varchar2(200),
   ReleaseDate date,
   HospitalName varchar2(100),
-  HospitalLocation varchar2(20),
+  HospitalDistrictName varchar2(20),
+  HospitalDistrictSize number,
   constraint PK_PATIENT PRIMARY KEY(DateOfAdmission, ReleaseDate),
-  CONSTRAINT FK_PATIENT_NID FOREIGN KEY(NID) REFERENCES CITIZEN(NID)
+  CONSTRAINT FK_PATIENT_NID FOREIGN KEY(NID) REFERENCES CITIZEN(NID),
+  constraint fk_patient_hospital FOREIGN KEY(HospitalName,HospitalDistrictName,HospitalDistrictSize) REFERENCES HOSPITAL(HospitalName,HospitalDistrictName,HospitalDistrictSize)
 );
 
--- Lab-07/DML.sql
+++ Lab-07/DML.sql
@@ -1,2 +1,13 @@
+
+--Task-1-- 
 SELECT * FROM DIVISION,(SELECT DivisionName,count(DivisionName)DistrictCount FROM DISTRICT GROUP BY DIVISION)DistrictDivision WHERE DIVISION.DivisionName=DistrictDivision.DivisionName;
+
+--Task-2--
+SELECT DISTRICT.DistrictName,count(DISTRICT.DistrictName)Population FROM DISTRICT,CITIZEN WHERE DISTRICT.DistrictName=CITIZEN.DistrictName AND DISTRICT.DistrictSize=CITIZEN.DistrictSize GROUP BY DISTRICT.DistrictName;
+
+--Task-3--
+SELECT count(*)NumberOfAccidents FROM ACCIDENT_LICENSE,CITIZEN WHERE ACCIDENT_LICENSE.NID='210' AND CITIZEN.NID='210' GROUP BY CITIZEN.NID;
+
+--Task-4--
+SELECT * FROM (SELECT HospitalName,count(*)NumberOfPatients FROM HOSPITAL,PATIENT WHERE HOSPITAL.Name=PATIENT.HospitalName GROUP BY HospitalName ORDER BY DESC) WHERE ROWNUM<=5;
 
