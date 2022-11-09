776d6d26a4b3e6a0d377cbfeb1576efe7643dacb
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-11-09 09:16:01 +0600

    Lab10 Task 5 completed for CSE 4308 DBMS

--- Lab-10 PL_SQL Tutorial #2/Lab10Task5.sql
+++ Lab-10 PL_SQL Tutorial #2/Lab10Task5.sql
@@ -1,14 +1,18 @@
+SET SERVEROUTPUT ON SIZE 1000000
+DROP TRIGGER ADVISOR_ASSIGN;
 CREATE OR REPLACE TRIGGER ADVISOR_ASSIGN
-BEFORE INSERT
-   ON ADVISOR
+AFTER INSERT
+   ON STUDENT
    FOR EACH ROW
+DECLARE
+   INS_ID advisor.i_id%TYPE;
 BEGIN
-   SELECT instructor.NEXTVAL INTO :NEW.i_ID FROM DUAL ;
-END;
+    select i_id INTO INS_ID from (select i_id from advisor WHERE s_id<>:new.ID) WHERE ROWNUM<=1;
+    insert into advisor(i_id,s_id) values(INS_ID,:new.ID);
+END ;
 /
 
-
 begin
-  insert into advisor values('1');
+  insert into student(NAME,DEPT_NAME,TOT_CRED) values ('Mirza', 'Comp. Sci.', '181');
 end;
-
+/
