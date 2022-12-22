a3b3a26ef9b206816ad399d2ded9b6af776ccc40
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-12-22 21:34:30 +0600

    Update to Quiz3

--- Quiz3AutomatedCandidateKeyDetermination/CandidateKeyDetermination/src/main/java/CandidateKeyDetermination/Main.java
+++ Quiz3AutomatedCandidateKeyDetermination/CandidateKeyDetermination/src/main/java/CandidateKeyDetermination/Main.java
@@ -1,8 +1,7 @@
 package CandidateKeyDetermination;
-
-import java.util.List;
-import java.util.Set;
-
+//Author: Mirza Mohammad Azwad
+//Student ID: 200042121
+//Github Link: https://github.com/mirzaazwad/CSE4307_DatabaseManagementSystem/tree/main/Quiz3AutomatedCandidateKeyDetermination
 public class Main {
     public static void main(String[] args)
     {
