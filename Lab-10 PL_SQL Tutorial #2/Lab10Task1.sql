b972d85f6f00570d8aec4c2397b7914e96ff00ba
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-12-22 21:29:22 +0600

    Modification to lab10

--- Lab-10 PL_SQL Tutorial #2/Lab10Task1.sql
+++ Lab-10 PL_SQL Tutorial #2/Lab10Task1.sql
@@ -4,13 +4,13 @@ PROCEDURE IncrementSalary
 AS
     INC NUMBER DEFAULT 0;
 BEGIN
-    UPDATE instructor SET salary=1.10*salary WHERE salary<75000;
     FOR i IN (SELECT * FROM instructor) LOOP
         IF i.salary<75000 THEN
             INC:=INC+1;
         END IF;
     END LOOP;
     dbms_output.put_line(INC);
+    UPDATE instructor SET salary=1.10*salary WHERE salary<75000;
 END;
 /
 
